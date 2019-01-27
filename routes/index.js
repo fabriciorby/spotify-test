aa1129bad107c88d7ecc6c2297c0b9521113b2e0
Fabrício Yamamoto <fabriciorby@hotmail.com>
2019-01-27 21:25:37 -0200

    cadastra e checa usuario no /callback

--- routes/index.js
+++ routes/index.js
@@ -17,35 +17,41 @@ router.get('/', async (req, res, next) => {
   res.render('logar', { title: title, spotifyAuthURL: authURL });
 });
 
-//chamado após a autenticação Spotify apenas para pegar o code
+//chamado após a autenticação Spotify: pega code e salva user no db
 router.get('/callback', async (req, res, next) => {
   let code = req.query.code;
+
   if (!spotifyApi.getRefreshToken())
     await spotifyHelper.getRefreshToken(code);
+  
+  let userSpotify = await spotifyHelper.getUserInfo();
+
+  let query = { id: { $eq: userSpotify.id } };
+  let update = { last_seen: new Date() };
+  let options = { new: true, returnNewDocument: true }
+
+  //check if user already exists, if so then update last_seen
+  userInfo = await User.findOneAndUpdate(query, update, options,
+    (err, data) => {
+      if (err) throw err;
+      return data;
+    });
+
+  //else, create new User on DB
+  if (!userInfo) {
+    userInfo = new User(userSpotify);
+    await userInfo.save((err) => {
+      if (err) throw err;
+    })
+  }
+
+  console.log(userInfo);
+
   res.redirect('index');
 });
 
 //finalmente redirecionado para cá após o login
 router.get('/index', async (req, res, next) => {
-
-  userInfo = await spotifyHelper.getUserInfo();
-
-  let query = { id: { $eq: userInfo.id } };
-  let update = { name: userInfo.name, email: userInfo.email, last_seen: new Date() };
-  let options = { upsert: true, new: true, setDefaultsOnInsert: true }
-
-  userInfo = await User.findOneAndUpdate(query, update, options,
-    (err, data) =>   {
-      if (err) throw err;
-      if (!data) data = new User(userInfo);
-      data.save((err) => {
-        if (err) throw err;
-        else return data;
-      });
-    });
-
-  console.log(userInfo);
-
   res.render('index', { title: title, user: userInfo });
 });
 
