0c24ef0b93f2d22ae56aded10a9f1aaf7c168dee
Fabrício Yamamoto <fabriciorby@hotmail.com>
2019-01-27 05:16:16 -0200

    adicionado menu e estilos

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ const router = express.Router();
 const spotifyApi = require('../spotifyConfig')
 const SpotifyHelper = require('../spotifyHelper');
 const spotifyHelper = new SpotifyHelper();
+const constants = require('../config/constants');
 
+let title = constants.title;
 let userInfo = {};
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   spotifyApi.setRedirectURI(req.protocol + '://' + req.get('host') + '/callback');
   let authURL = await spotifyHelper.getAuthURL();
-  res.render('logar', { title: 'Teste' , spotifyAuthURL: authURL});
+  res.render('logar', { title: title , spotifyAuthURL: authURL});
 });
 
 //chamado após a autenticação Spotify apenas para pegar o code
@@ -25,19 +27,19 @@ router.get('/callback', async (req, res, next) => {
 router.get('/index', async (req,res,next) => {
   userInfo = await spotifyHelper.getUserInfo();
   console.log(userInfo);
-  res.render('index', { title: 'Teste', user: userInfo});
+  res.render('index', { title: title, user: userInfo});
 });
 
 //finalmente redirecionado para cá após o login
 router.get('/conteudo', async (req,res,next) => {
   let listAlbum = await spotifyHelper.getAlbum();
-  res.render('conteudo', { title: 'Teste', list: listAlbum, user: userInfo});
+  res.render('conteudo', { title: title, list: listAlbum, user: userInfo});
 });
 
 //search for artist, album, track
 router.get('/search', async (req,res,next) => {
   let listData = await spotifyHelper.searchData(req.query.tipo, req.query.nome);
-  res.render('conteudo', { title: 'Teste', list: listData, user: userInfo});
+  res.render('conteudo', { title: title, list: listData, user: userInfo});
 });
 
 router.get('/error', function(req, res, next) {
